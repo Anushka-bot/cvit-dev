e5110a054a0ac63ac10278606dcdc9e57f0fb084
Anushka <75663460+Anushka-bot@users.noreply.github.com>
2023-06-22 14:27:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,30 @@
 from flask import Flask, render_template, request, redirect, url_for
-import subprocess
+import subprocess,os
+from werkzeug.utils import secure_filename
 
 app=Flask(__name__)
+
+url="https://xyz.com/"
 
 @app.route("/")
 def home():
     return render_template("home.html")
 
-@app.route("/input")
-def input():
-    return render_template("input.html")
-
 @app.route("/process", methods=['POST'])
 def process():
     if request.method=="POST":
-        path=request.form['path']
-        name=request.form['name']
-        if path:
-            path = '@'+path
-            if not name:
-                name="output.txt"
-            command = 'curl -X POST -F "video={}" -F "name={}" {}'
-            link='ng-rok-link/lipreading'
-            subprocess.call(command.format(path,name,link),shell=True)
+        f=request.files['video']
+        file_name = secure_filename(f.filename)
+        filepath = os.path.join("static/",file_name)
+        f.save(filepath)
+        if f:
+            path = '@'+ filepath
+            link=f'{url}lipreading'
+            command = f'curl -X POST -F "video={path}" {link}'
+            print(command)
+            #subprocess.call(command.format(path,link),shell=True)
     return redirect(url_for("home"))
 
 if __name__=="__main__":
     app.run(debug=True)
+
